ea2143030dc49516ed29ec3ceff6d700f5664624
Daniel Lauermann <skrattter@googlemail.com>
2019-07-18 23:08:51 +0200

    no bootstrap anymore, tiles with css grid

--- src/components/pages/Test.vue
+++ src/components/pages/Test.vue
@@ -1,70 +1,22 @@
 <template>
-    <div class="container">
-        <b-row>
-            <b-col cols="6" sm="4">
-                <b-row>
-                    <b-col cols="12">
-                        <div class="tile" style="margin: 5px;">
-                        </div>
-                    </b-col>
-                    <b-col cols="12">
-                        <div class="tile" style="margin: 5px;">
-                        </div>
-                    </b-col>
-                    <b-col cols="6">
-                        <div class="tile" style="margin: 5px;">
-                        </div>
-                    </b-col>
-                    <b-col cols="6">
-                        <div class="tile" style="margin: 5px;">
-                        </div>
-                    </b-col>
-                </b-row>
-            </b-col>
-        </b-row>
-
-        <div class="row">
-            <div class="col-6 col-sm-4 col-md-3 col-lg-2  col-xl-2">
-                <div class="tile" style="margin: 5px;">
-                </div>
-            </div>
-            <div class="col-6 col-sm-4 col-md-3 col-lg-2  col-xl-2">
-                <div class="tile" style="margin: 5px;">
-                </div>
-            </div>
-            <div class="col-6 col-sm-4 col-md-3 col-lg-2  col-xl-2">
-                <div class="tile" style="margin: 5px;">
-                </div>
-            </div>
-            <div class="col-6 col-sm-4 col-md-3 col-lg-2  col-xl-2">
-                <div class="tile" style="margin: 5px;">
-                </div>
-            </div>
-            <div class="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2">
-                <div class="tile" style="margin: 5px;">
-                </div>
-            </div>
-            <div class="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2">
-                <div class="tile" style="margin: 5px;">
-                </div>
-            </div>
-            <div class="col-12 col-sm-8 col-md-6 col-lg-4 col-xl-4">
-                <div class="tile tile-wide" style="margin: 5px;">
-                </div>
-            </div>
-            <div class="col-6 col-sm-4 col-md-3 col-lg-4  col-xl-4">
-                <div class="tile tile-wide" style="margin: 5px;">
-                </div>
-            </div>
-            <div class="col-6 col-sm-4 col-md-3 col-lg-2  col-xl-2">
-                <div class="tile" style="margin: 5px;">
-                </div>
-            </div>
-            <div class="col-6 col-sm-4 col-md-3 col-lg-2  col-xl-2">
-                <div class="tile" style="margin: 5px;">
-                </div>
-            </div>
+    <div class="grid">
+        <div class="grid-item orange">
+            <v-icon>fa fa-bus fa-fw</v-icon>
         </div>
+        <div class="grid-item red"></div>
+        <div class="grid-item green"></div>
+        <div class="grid-item yellow"></div>
+        <div class="grid-item brown"></div>
+        <div class="grid-item grid-item-w blue"></div>
+        <div class="grid-item purple"></div>
+        <div class="grid-item black"></div>
+        <div class="grid-item grid-item-h yellow"></div>
+        <div class="grid-item green"></div>
+        <div class="grid-item yellow"></div>
+        <div class="grid-item cyan"></div>
+        <div class="grid-item red"></div>
+        <div class="grid-item orange"></div>
+        <div class="grid-item blue"></div>
     </div>
 </template>
 
@@ -76,6 +28,7 @@ export default {
     },
     computed: {},
     mounted () {
+
     },
     beforeCreate () { },
     created () { },
@@ -84,15 +37,46 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.tile {
-    background: white;
-    width: 150px;
-    height: 150px;
+.grid {
+    display: grid;
+    grid-gap: 0;
+    // grid-auto-flow: dense;
+    grid-template-columns: repeat(auto-fit, 180px);
+    // grid-template-rows: repeat(auto, 180px);
+
+    // grid-template-rows: 180px;
+    grid-auto-rows: 180px;
+    margin: 5% auto;
 }
-.tile-wide {
-    width: 310px;
+.grid-item-w {
+    grid-column: span 2;
 }
-.tile-height {
-    height: 310px;
+.grid-item-h {
+    grid-row: span 2;
+}
+@media (max-width: 359px) {
+    .grid {
+        width: 180px;
+    }
+}
+@media (min-width: 360px) {
+    .grid {
+        width: 360px;
+    }
+}
+@media (min-width: 540px) {
+    .grid {
+        width: 540px;
+    }
+}
+@media (min-width: 720px) {
+    .grid {
+        width: 720px;
+    }
+}
+@media (min-width: 900px) {
+    .grid {
+        width: 900px;
+    }
 }
 </style>
